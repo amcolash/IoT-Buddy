20799825b43ffd8b943b41cfa086649764237c89
Andrew McOlash <amcolash@gmail.com>
2016-01-05 01:00:33 -0600

    More work on the web config

--- web_config/app.js
+++ web_config/app.js
@@ -2,10 +2,44 @@ var triggers = [{title: 'Toggle Lights', trigger: 'toggle_lights'}];
 var options = {triggers};
 //document.location = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(options));
 
-$(document).ready(function() {
-  $('.item-draggable-list label').append('<span class="close">X</span>');
+function closeHandler() {
+  $('.close').unbind('click');
 
   $('.close').click(function() {
     $(this).parent().remove();
+
+    if ($('.item-draggable-list label').length === 0) {
+      $('.item-draggable-list').append(
+        '<span class="item empty">Trigger List Empty</span>'
+      );
+    }
+  });
+}
+
+$(document).ready(function() {
+  $('.item-draggable-handle').remove();
+
+  $('#add').click(function() {
+    var trigger_name = $('input[name="trigger-name"]').val();
+    var trigger_event = $('input[name="trigger-event"]').val();
+
+    if (trigger_name === '' || trigger_event === '') {
+      alert('Need a name and event!');
+      return;
+    }
+
+    $('.item-draggable-list').append(
+      '<label class="item">' + trigger_name + ' - ' + trigger_event +
+        '<span class="close">X</span>' +
+        '<div class="item-draggable-handle">' +
+          '<div class="item-draggable-handle-bar"></div>' +
+          '<div class="item-draggable-handle-bar"></div>' +
+          '<div class="item-draggable-handle-bar"></div>' +
+        '</div>' +
+      '</label>'
+    );
+    $('.empty').remove();
+
+    closeHandler();
   });
 });
