091fe6de073463aae8a00cb1cc777b871902ff26
Andrew McOlash <amcolash@gmail.com>
2016-01-05 00:24:39 -0600

    Worked on the web config page

--- web_config/app.js
+++ web_config/app.js
@@ -1,3 +1,11 @@
 var triggers = [{title: 'Toggle Lights', trigger: 'toggle_lights'}];
 var options = {triggers};
-document.location = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(options));
+//document.location = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(options));
+
+$(document).ready(function() {
+  $('.item-draggable-list label').append('<span class="close">X</span>');
+
+  $('.close').click(function() {
+    $(this).parent().remove();
+  });
+});
