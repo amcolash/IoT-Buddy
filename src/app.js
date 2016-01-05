083967442a7b8f6d3fed7f5294a3fcc5e132133c
Andrew McOlash <amcolash@gmail.com>
2016-01-04 22:28:38 -0600

    Started working on settings for app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
-
 var UI = require('ui');
 var ajax = require('ajax');
+var Settings = require('settings');
 
 var serverUrl = 'https://maker.ifttt.com/trigger/';
 var keyPrefix = '/with/key/';
 var key = 'QW9btRcieq0Z83x0Z7tRQ';
 
-// Make a list of menu items
-var triggers = [
+// Set a configurable with the open callback
+Settings.config(
+  { url: 'http://pi.amcolash.com/pebbleiot/index.html' },
+  function(e) {
+    console.log('opening configurable');
+  },
+  function(e) {
+    console.log('closed configurable');
+  }
+);
+
+var triggers1 = [
   {
     title: 'Toggle Lights',
     trigger: 'toggle_lights'
@@ -18,21 +28,33 @@ var triggers = [
   }
 ];
 
+Settings.option('triggers', triggers1);
+var triggers = Settings.option('triggers');
 // Create the Menu, supplying the list of fruits
-var triggerMenu = new UI.Menu({
-  sections: [{
-    title: 'Trigger List',
-    items: triggers
-  }]
-});
 
-// Show the Menu
-triggerMenu.show();
-
-// Add a click listener for select button click
-triggerMenu.on('select', function(event) {
-  ajax({
-    url: serverUrl + triggers[event.itemIndex].trigger + keyPrefix + key,
-    method: 'put'
+if (triggers !== null && triggers.length !== 0) {
+  var triggerMenu = new UI.Menu({
+    sections: [{
+      title: 'Trigger List',
+      items: triggers
+    }]
   });
-});
+  
+  // Show the Menu
+  triggerMenu.show();
+  
+  // Add a click listener for select button click
+  triggerMenu.on('select', function(event) {
+    ajax({
+      url: serverUrl + triggers[event.itemIndex].trigger + keyPrefix + key,
+      method: 'put'
+    });
+  });
+} else {
+  var card = new UI.Card({
+    title: 'Set up app',
+    body: 'You need to set up PebbleIoT from your phone.'
+  });
+  
+  card.show();
+}
