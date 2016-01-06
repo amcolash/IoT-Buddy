cc52d3b0387a8cb393a1bcb685279f3c9f83745b
Andrew McOlash <amcolash@gmail.com>
2016-01-05 21:37:08 -0600

    All settings loaded correctly now

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var Settings = require('settings');
 
 var serverUrl = 'https://maker.ifttt.com/trigger/';
 var keyPrefix = '/with/key/';
-var key = 'QW9btRcieq0Z83x0Z7tRQ';
+var key;
 
 // Set a configurable with the open callback
 Settings.config(
@@ -16,35 +16,46 @@ Settings.config(
   function(e) {
     console.log('closed configurable');
     console.log(JSON.stringify(e));
+    refreshMenu();
   }
 );
 
-var triggers = Settings.option('triggers');
-// Create the Menu, supplying the list of fruits
+refreshMenu();
 
-if (triggers !== null && triggers.length !== 0) {
-  var triggerMenu = new UI.Menu({
-    sections: [{
-      title: 'Trigger List',
-      items: triggers
-    }]
-  });
+function refreshMenu() {
+  var triggers = Settings.option('triggers');
+  key = Settings.option('key');
+  var menuItems = [];
+  if (triggers !== null) {
+    for (var i = 0; i < triggers.length; i++) {
+      menuItems.push({'title': triggers[i].trigger_name, 'subtitle': triggers[i].trigger_event});
+    }
+  }
   
-  // Show the Menu
-  triggerMenu.show();
-  
-  // Add a click listener for select button click
-  triggerMenu.on('select', function(event) {
-    ajax({
-      url: serverUrl + triggers[event.itemIndex].trigger + keyPrefix + key,
-      method: 'put'
+  if (menuItems.length > 0 && key !== '') {
+    var triggerMenu = new UI.Menu({
+      sections: [{
+        title: 'Trigger List',
+        items: menuItems
+      }]
     });
-  });
-} else {
-  var card = new UI.Card({
-    title: 'Set up app',
-    body: 'You need to set up PebbleIoT from your phone.'
-  });
-  
-  card.show();
+    
+    // Show the Menu
+    triggerMenu.show();
+    
+    // Add a click listener for select button click
+    triggerMenu.on('select', function(event) {
+      ajax({
+        url: serverUrl + menuItems[event.itemIndex].subtitle + keyPrefix + key,
+        method: 'put'
+      });
+    });
+  } else {
+    var card = new UI.Card({
+      title: 'Set up app',
+      body: 'You need to set up PebbleIoT from your phone.'
+    });
+    
+    card.show();
+  }
 }
