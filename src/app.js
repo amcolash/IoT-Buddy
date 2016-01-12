72c4d29f70e6e9ac20ab043906d69faf20d890ce
Andrew McOlash <amcolash@gmail.com>
2016-01-12 00:21:29 -0600

    Fix problems with web config stuff

--- src/app.js
+++ src/app.js
@@ -6,12 +6,15 @@ var serverUrl = 'https://maker.ifttt.com/trigger/';
 var keyPrefix = '/with/key/';
 var key;
 
+var triggerMenu;
+
 // Set a configurable with the open callback
 Settings.config(
-  { url: 'http://pi.amcolash.com/pebbleiot/index.html?' +
+  { url: 'http://pi.amcolash.com/pebbleiot/index.html?'  +
    encodeURIComponent(JSON.stringify(Settings.option())), autoSave: true },
   function(e) {
     console.log('opening configurable');
+    console.log(JSON.stringify(Settings.option()));
   },
   function(e) {
     console.log('closed configurable');
@@ -26,14 +29,20 @@ function refreshMenu() {
   var triggers = Settings.option('triggers');
   key = Settings.option('key');
   var menuItems = [];
-  if (triggers !== null) {
+  if (triggers !== null && triggers instanceof Array) {
+    // Parse options and turn it into a list of menu items
     for (var i = 0; i < triggers.length; i++) {
       menuItems.push({'title': triggers[i].trigger_name, 'subtitle': triggers[i].trigger_event});
     }
   }
   
   if (menuItems.length > 0 && key !== '') {
-    var triggerMenu = new UI.Menu({
+    // Possibly remove old menu and make a new one
+    if (triggerMenu !== null && triggerMenu !== undefined) {
+      triggerMenu.hide();
+    }
+    
+    triggerMenu = new UI.Menu({
       sections: [{
         title: 'Trigger List',
         items: menuItems
@@ -53,7 +62,7 @@ function refreshMenu() {
   } else {
     var card = new UI.Card({
       title: 'Set up app',
-      body: 'You need to set up PebbleIoT from your phone.'
+      body: 'You need to set up IoT Buddy from your phone.'
     });
     
     card.show();
