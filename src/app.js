e59c1b067d03b0afd85ff9a8ff1875e772f40a24
Andrew McOlash <amcolash@gmail.com>
2016-01-25 20:06:35 -0600

    Lots of changes - iOS init, color, vibration

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ var key;
 var triggerMenu;
 var card;
 
+var started = false;
+var color = true;
+
 // Set a configurable with the open callback
 Settings.config(
   { url: 'http://pi.amcolash.com/pebbleiot/index.html?'  +
@@ -25,8 +28,17 @@ Settings.config(
   }
 );
 
-Pebble.addEventListener("ready", function() {
+// Seems to work better for iOS?
+if (!started) {
   refreshMenu();
+  color = Pebble.getActiveWatchInfo().platform !== 'aplite';
+}
+
+// Both shouldn't fire
+Pebble.addEventListener('ready', function(e) {
+  if (!started) {
+    refreshMenu();
+  }
 });
                         
 function refreshMenu() {
@@ -53,10 +65,23 @@ function refreshMenu() {
       sections: [{
         title: 'Trigger List',
         items: menuItems
-      }]
+      }],
     });
     
+    if (color) {
+      var backgroundColor = Settings.option('backgroundColor');
+      var highlightBackgroundColor = Settings.option('highlightBackgroundColor');
+      var textColor = Settings.option('textColor');
+      var highlightTextColor = Settings.option('highlightTextColor');
+      
+      triggerMenu.backgroundColor = backgroundColor || 'white';
+      triggerMenu.highlightBackgroundColor = highlightBackgroundColor || 'black';
+      triggerMenu.textColor = textColor || 'black';
+      triggerMenu.highlightTextColor = highlightTextColor || 'white';
+    }
+    
     // Show the Menu
+    console.log('Showing main menu');
     triggerMenu.show();
     
     // Add a click listener for select button click
@@ -68,6 +93,8 @@ function refreshMenu() {
       });
     });
   } else {
+    console.log('Setup Required');
+    
     card = new UI.Card({
       title: 'Set up app',
       body: 'You need to set up IoT Buddy from your phone.'
