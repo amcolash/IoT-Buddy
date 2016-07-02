25d574ba51067f2d685f1d29f744bba2325ef593
Andrew McOlash <amcolash@gmail.com>
2016-07-02 12:30:02 -0500

    Add in menu layer

--- src/main.c
+++ src/main.c
@@ -1,42 +1,76 @@
 #include <pebble.h>
 
 static Window *s_main_window;
-static TextLayer *s_time_layer;
+static MenuLayer *s_menu_layer;
+
+uint16_t num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *callback_context) {
+  return 3;
+}
+
+void draw_row_callback(GContext *ctx, Layer *cell_layer, MenuIndex *cell_index, void *callback_context) {
+  //Which row is it?
+  switch(cell_index->row) {
+    case 0:
+      menu_cell_basic_draw(ctx, cell_layer, "1. Apple", "Green and crispy!", NULL);
+      break;
+    case 1:
+      menu_cell_basic_draw(ctx, cell_layer, "2. Orange", "Peel first!", NULL);
+      break;
+    case 2:
+      menu_cell_basic_draw(ctx, cell_layer, "3. Pear", "Teardrop shaped!", NULL);
+      break;
+  }
+}
+  
+void select_click_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) {
+  //Get which row
+  int which = cell_index->row;
+
+  //The array that will hold the on/off vibration times
+  uint32_t segments[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+  //Build the pattern (milliseconds on and off in alternating positions)
+  for(int i = 0; i < which + 1; i++)
+  {
+    segments[2 * i] = 200;
+    segments[(2 * i) + 1] = 100;
+  }
+
+  //Create a VibePattern data structure
+  VibePattern pattern = {
+    .durations = segments,
+    .num_segments = 16
+  };
+
+  //Do the vibration pattern!
+  vibes_enqueue_custom_pattern(pattern);
+}
 
 static void main_window_load(Window *window) {
-  // Get information about the Window
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+  //Create it - 12 is approx height of the top bar
+  s_menu_layer = menu_layer_create(GRect(0, 0, 144, 168 - 16));
 
-  // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+  //Let it receive clicks
+  menu_layer_set_click_config_onto_window(s_menu_layer, window);
 
-  // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  //Give it its callbacks
+  MenuLayerCallbacks callbacks = {
+    .draw_row = (MenuLayerDrawRowCallback) draw_row_callback,
+    .get_num_rows = (MenuLayerGetNumberOfRowsInSectionsCallback) num_rows_callback,
+    .select_click = (MenuLayerSelectCallback) select_click_callback
+  };
+  menu_layer_set_callbacks(s_menu_layer, NULL, callbacks);
 
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  //Add to Window
+  layer_add_child(window_get_root_layer(window), menu_layer_get_layer(s_menu_layer));
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy TextLayer
-  text_layer_destroy(s_time_layer);
+  // Destroy MenuLayer
+  menu_layer_destroy(s_menu_layer);
 }
 
 static void inbox_received_handler(DictionaryIterator *iter, void *context) {
-  // Read color preferences
-  text_layer_set_text(s_time_layer, "00:00");
-
-  // GColor bg_color = GColor_Black;
-  // GColor fg_color = GColor_White;
-  bool second_ticks = 0;
-  bool animations = 0;
-
   Tuple *bg_color_t = dict_find(iter, MESSAGE_KEY_BackgroundColor);
   if(bg_color_t) {
     GColor bg_color = GColorFromHEX(bg_color_t->value->int32);
@@ -49,20 +83,12 @@ static void inbox_received_handler(DictionaryIterator *iter, void *context) {
 
   Tuple *second_tick_t = dict_find(iter, MESSAGE_KEY_SecondTick);
   if(second_tick_t) {
-    second_ticks = second_tick_t->value->int32 == 1;
-    if (second_ticks) {
-      text_layer_set_text(s_time_layer, "11:11");
-    }
+    bool second_ticks = second_tick_t->value->int32 == 1;
   }
 
   Tuple *animations_t = dict_find(iter, MESSAGE_KEY_Animations);
   if(animations_t) {
-    animations = animations_t->value->int32 == 1;
-    if (animations && second_ticks) {
-      text_layer_set_text(s_time_layer, "33:33");
-    } else if (animations) {
-      text_layer_set_text(s_time_layer, "22:22");
-    }
+    bool animations = animations_t->value->int32 == 1;
   }
 }
 
