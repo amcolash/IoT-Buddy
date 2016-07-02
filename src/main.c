efba22ce3f709c20304c991867670e0234577f49
Andrew McOlash <amcolash@gmail.com>
2016-07-02 11:48:37 -0500

    Start playing around with c and clay

--- src/main.c
+++ src/main.c
@@ -28,6 +28,49 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
 }
 
+static void inbox_received_handler(DictionaryIterator *iter, void *context) {
+  // Read color preferences
+  text_layer_set_text(s_time_layer, "00:00");
+
+  // GColor bg_color = GColor_Black;
+  // GColor fg_color = GColor_White;
+  bool second_ticks = 0;
+  bool animations = 0;
+
+  Tuple *bg_color_t = dict_find(iter, MESSAGE_KEY_BackgroundColor);
+  if(bg_color_t) {
+    GColor bg_color = GColorFromHEX(bg_color_t->value->int32);
+  }
+
+  Tuple *fg_color_t = dict_find(iter, MESSAGE_KEY_ForegroundColor);
+  if(fg_color_t) {
+    GColor fg_color = GColorFromHEX(fg_color_t->value->int32);
+  }
+
+  Tuple *second_tick_t = dict_find(iter, MESSAGE_KEY_SecondTick);
+  if(second_tick_t) {
+    second_ticks = second_tick_t->value->int32 == 1;
+    if (second_ticks) {
+      text_layer_set_text(s_time_layer, "11:11");
+    }
+  }
+
+  Tuple *animations_t = dict_find(iter, MESSAGE_KEY_Animations);
+  if(animations_t) {
+    animations = animations_t->value->int32 == 1;
+    if (animations && second_ticks) {
+      text_layer_set_text(s_time_layer, "33:33");
+    } else if (animations) {
+      text_layer_set_text(s_time_layer, "22:22");
+    }
+  }
+}
+
+void inbox_init() {
+  app_message_register_inbox_received(inbox_received_handler);
+  app_message_open(128, 128);
+}
+
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
@@ -40,6 +83,8 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+
+  inbox_init();
 }
 
 static void deinit() {
@@ -51,38 +96,4 @@ int main(void) {
   init();
   app_event_loop();
   deinit();
-}
-
-
-static void prv_inbox_received_handler(DictionaryIterator *iter, void *context) {
-  // Read color preferences
-  Tuple *bg_color_t = dict_find(iter, MESSAGE_KEY_BackgroundColor);
-  if(bg_color_t) {
-    GColor bg_color = GColorFromHEX(bg_color_t->value->int32);
-    APP_LOG(APP_LOG_LEVEL_DEBUG, bg_color);
-  }
-
-  Tuple *fg_color_t = dict_find(iter, MESSAGE_KEY_ForegroundColor);
-  if(fg_color_t) {
-    GColor fg_color = GColorFromHEX(fg_color_t->value->int32);
-    APP_LOG(APP_LOG_LEVEL_DEBUG, fg_color);
-  }
-
-  // Read boolean preferences
-  Tuple *second_tick_t = dict_find(iter, MESSAGE_KEY_SecondTick);
-  if(second_tick_t) {
-    bool second_ticks = second_tick_t->value->int32 == 1;
-    APP_LOG(APP_LOG_LEVEL_DEBUG, second_ticks);
-  }
-
-  Tuple *animations_t = dict_find(iter, MESSAGE_KEY_Animations);
-  if(animations_t) {
-    bool animations = animations_t->value->int32 == 1;
-    APP_LOG(APP_LOG_LEVEL_DEBUG, animations);
-  }
-}
-
-void prv_init(void) {
-  app_message_register_inbox_received(prv_inbox_received_handler);
-  app_message_open(128, 128);
 }
